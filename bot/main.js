951833a65cda14b8bf45573897ecd937eea0457b
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2020-04-19 10:18:49 -0300

    /help command

--- bot/main.js
+++ bot/main.js
@@ -13,6 +13,14 @@ bot.start(async context => {
 
 });
 
+bot.help(context => {
+    let messageContent = `I am a simple dockerized version of Echo Bot (https://github.com/macielti/EchoBotDocker).
+    I will reply to every message that you send to me with the same content that you sended.`;
+    context.reply(messageContent);
+
+    logMessage(context);
+});
+
 bot.on('text', async context => {
     let messageTextContent = await context.update.message.text;
     context.reply(messageTextContent);
