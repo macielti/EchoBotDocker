08a754472a34fe8a59502df49c366f25bf2578ea
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2020-04-19 09:47:46 -0300

    added logs funcionality on server side.

--- bot/main.js
+++ bot/main.js
@@ -1,11 +1,24 @@
 require('dotenv').config();
 const Telegraf = require('telegraf');
+const log4js = require('log4js');
+
+const logger = log4js.getLogger();
+logger.level = 'info';
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.on('text', context => {
-        let messageTextContent = context.update.message.text;
+bot.on('text', async context => {
+        let messageTextContent = await context.update.message.text;
         context.reply(messageTextContent);
+
+        // first name of the sender user
+        let firstNameFrom = context.update.message.from.first_name;
+        // username of the sender user
+        let username = context.update.message.from.username;
+        // select username or first name
+        let userInfo = username? username:firstNameFrom;
+
+        logger.info(`Echoed to ${userInfo}: ${messageTextContent}`);
     }
 );
 
